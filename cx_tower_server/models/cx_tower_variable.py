fdb0486f7b2e9cf716f6d09498db917696ccb01e
i1 <ivan@cetmix.com>
2022-06-18 20:40:04 +0200

    [IMP] "globa;" setting for variable values

--- cx_tower_server/models/cx_tower_variable.py
+++ cx_tower_server/models/cx_tower_variable.py
@@ -4,5 +4,6 @@ from odoo import _, api, fields, models
 class TowerVariable(models.Model):
     _name = "cx.tower.variable"
     _description = "Cetmix Tower Variable"
+    _order = "name"
 
     name = fields.Char(string="Name", required=True)
