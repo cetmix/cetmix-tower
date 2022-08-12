64d1d2eb0aeb0b58ffca23687fba53c6f547e649
Ivan Sokolov <jzx115@gmail.com>
2022-08-12 10:36:41 +0200

    [IMP] cx_tower_server (#9)
    - BREAKING: module renamed: cx_tower_server --> cetmix_tower_server
    - Demo data for core models
    
    cx_tower_server: "Use sudo" field in model and view
    
    cx.tower.server: 'execute_commands' method for running multiple commands for multiple servers
    
    cx.tower.server: run commands using sudo. IMPORTANT: keep an eye on docs!
    
    cx.tower.command.execute.wizard: button for running command using cx.tower.server 'execute_commands' method
    
    cx.tower.server: tests
    
    cx_tower_variable_mixin: unlink variable values when records they belong to are unlinked
    
    cx_tower_variable_value: test coverage for unlink
    
    cx_tower_server_variable: prettify value popup o2m form
    
    cx_tower_server_variable_value: tree view and filters update
    
    cx_tower_command: added "active" field, done search and tree view polishing
    
    cx_tower_variable_value: view fixes

---
+++ cetmix_tower_server/__manifest__.py
@@ -0,0 +1,38 @@
+# Copyright Cetmix OU
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Cetmix Tower Server Management",
+    "summary": "Manage remote servers directly from Odoo",
+    "version": "14.0.0.2.0",
+    "category": "Productivity",
+    "website": "https://cetmix.com",
+    "author": "Cetmix",
+    "license": "LGPL-3",
+    "application": True,
+    "installable": True,
+    "external_dependencies": {
+        "python": ["paramiko", "scp"],
+        "bin": [],
+    },
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/cx_tower_server_security.xml",
+        "security/ir.model.access.csv",
+        "data/ir_actions_server.xml",
+        "views/cx_tower_server_view.xml",
+        "views/cx_tower_os_view.xml",
+        "views/cx_tower_tag_view.xml",
+        "views/cx_tower_interpreter_view.xml",
+        "views/cx_tower_variable_view.xml",
+        "views/cx_tower_variable_value_view.xml",
+        "views/cx_tower_command_view.xml",
+        "views/cx_tower_key_view.xml",
+        "views/menuitems.xml",
+        "wizards/cx_tower_command_execute_wizard_view.xml",
+    ],
+    "demo": [
+        "data/demo_data.xml",
+    ],
+}
---
+++ cetmix_tower_server/wizards/cx_tower_command_execute_wizard.py
@@ -0,0 +1,172 @@
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class CxTowerCommandExecuteWizard(models.TransientModel):
+    _name = "cx.tower.command.execute.wizard"
+    _inherit = "cx.tower.template.mixin"
+    _description = "Execute Command in Wizard"
+
+    server_ids = fields.Many2many(
+        "cx.tower.server",
+        string="Servers",
+    )
+    command_id = fields.Many2one(
+        "cx.tower.command",
+        required=True,
+    )
+    tag_ids = fields.Many2many(
+        comodel_name="cx.tower.tag",
+        relation="cx_tower_command_execute_tag_rel",
+        column1="wizard_id",
+        column2="tag_id",
+        string="Tags",
+    )
+    any_server = fields.Boolean()
+    rendered_code = fields.Text()
+    result = fields.Text()
+
+    @api.onchange("command_id")
+    def _onchange_command_id(self):
+        """
+        Set code after change command
+        """
+        if self.command_id and self.server_ids:
+            self.code = self.command_id.code
+
+    @api.onchange("code", "server_ids")
+    def _onchange_code(self):
+        if self.server_ids:
+            server_id = self.server_ids[0]  # TODO testing only!!!
+
+            # Get variable list
+            variables = self.get_variables()
+
+            # Get variable values
+            variable_values = server_id.get_variable_values(variables.get(self.id))
+
+            # Render template
+            if variable_values:
+                self.rendered_code = self.render_code(
+                    **variable_values.get(server_id.id)
+                ).get(self.id)
+            else:
+                self.rendered_code = self.code
+
+    @api.onchange("any_server", "server_ids", "tag_ids")
+    def _onchange_tag_ids(self):
+        """Compose domain based on condition
+        - any server: show commands compatible with any server
+        """
+
+        domain = []
+        if self.any_server:
+            domain = [("server_ids", "=", False)]
+        elif self.server_ids:
+            domain.append(("server_ids", "in", self.server_ids.ids))
+        if self.tag_ids:
+            domain.append(("tag_ids", "in", self.tag_ids.ids))
+
+        return {"domain": {"command_id": domain}}
+
+    def execute_command_on_server(self):
+        """Render selected command using server method"""
+        command_res = self.server_ids.execute_commands(self.command_id)
+        result = ""
+        for server in self.server_ids:
+            # Get result for this server and command from the res dict
+            status, response, error = command_res.get(server.id).get(self.command_id.id)
+            for err in error:
+                result += "[{server}]: ERROR: {err}".format(server=server.name, err=err)
+            for res in response:
+                result += "[{server}]: {res}".format(server=server.name, res=res)
+            if not result.endswith("\n"):
+                result += "\n"
+            result += "\n"
+
+        if result:
+            self.result = result
+            return {
+                "type": "ir.actions.act_window",
+                "name": _("Execute Result"),
+                "res_model": "cx.tower.command.execute.wizard",
+                "res_id": self.id,
+                "view_mode": "form",
+                "view_type": "form",
+                "target": "new",
+            }
+
+    def execute_command(self):
+        """
+        Executes a given code
+        """
+        self.ensure_one()
+        code = self.code
+        if not code:
+            raise ValidationError(_("You cannot execute empty command."))
+
+        # check that we can execute the command for selected servers
+        command_servers = self.command_id.server_ids
+        if command_servers and not all(
+            [server in command_servers for server in self.server_ids]
+        ):
+            raise ValidationError(_("Some servers don't support this command."))
+
+        result = ""
+
+        variables = self.get_variables()
+        # Get variable values
+        variable_values = self.server_ids.get_variable_values(variables.get(self.id))
+
+        for server in self.server_ids:
+
+            # Render template with values
+            if variable_values:
+                rendered_code = self.render_code(**variable_values.get(server.id)).get(
+                    self.id
+                )
+            else:
+                rendered_code = self.code
+
+            server_name = server.name
+            client = server._connect(raise_on_error=True)
+            status, response, error = server._execute_command(client, rendered_code)
+            for err in error:
+                result += "[{server}]: ERROR: {err}".format(server=server_name, err=err)
+            for res in response:
+                result += "[{server}]: {res}".format(server=server_name, res=res)
+            if not result.endswith("\n"):
+                result += "\n"
+            result += "\n"
+
+        if result:
+            self.result = result
+            return {
+                "type": "ir.actions.act_window",
+                "name": _("Execute Result"),
+                "res_model": "cx.tower.command.execute.wizard",
+                "res_id": self.id,
+                "view_mode": "form",
+                "view_type": "form",
+                "target": "new",
+            }
+
+    def action_execute_command(self):
+        """
+        Return wizard action to select command and execute it
+        """
+        context = self.env.context.copy()
+        context.update(
+            {
+                "default_server_ids": self.server_ids.ids,
+            }
+        )
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Execute Command"),
+            "res_model": "cx.tower.command.execute.wizard",
+            "view_mode": "form",
+            "view_type": "form",
+            "target": "new",
+            "context": context,
+        }
