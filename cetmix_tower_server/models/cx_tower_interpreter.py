d99bddf4d053a3aef589ec23e435bbd7e2227a8f
Ivan Sokolov <jzx115@gmail.com>
2022-09-09 19:10:55 +0200

    14.0 cetmix tower server fixes (#13)
    * server configuration:
    - Dockerfile
    - odoo.conf
    - nginx installation script
    
    * cx.tower.server.key: fix value retrieval issue
    
    * cx.tower.command.log: new filters in search view, change tab order in form view
    
    * cx.tower.command.log: fix 'labeled' filter
    
    * cx.tower.command.wizard: make view more convenient
    
    * cx.tower.command: list view improvements
    
    * cx.tower.server, cx.tower.tag, cx.tower.os, cx.tower.interpreter: 'color' field for better usability

--- cetmix_tower_server/models/cx_tower_interpreter.py
+++ cetmix_tower_server/models/cx_tower_interpreter.py
@@ -6,4 +6,5 @@ class CxTowerInterpreter(models.Model):
     _description = "Cetmix Tower Interpreter"
 
     name = fields.Char()
+    color = fields.Integer(help="For better visualization in views")
     path = fields.Char()
---
+++ docker/nginx_setup.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+################################################################################
+# Based on Odoo installation script by Yenthe Van Ginneken
+#-------------------------------------------------------------------------------
+# Make a new file:
+# sudo nano odoo-install.sh
+# Place this content in it and then make the file executable:
+# sudo chmod +x odoo-install.sh
+# Execute the script to install Odoo:
+# ./odoo-install
+################################################################################
+
+OE_USER="tower"
+OE_PORT="8069"
+# Set this to True if you want to install Nginx!
+INSTALL_NGINX="True"
+# Set the website name
+WEBSITE_NAME="tower.cetmix.com"
+# Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
+LONGPOLLING_PORT="8072"
+# Set to "True" to install certbot and have ssl enabled, "False" to use http
+ENABLE_SSL="True"
+# Provide Email to register ssl certificate
+ADMIN_EMAIL="tower@cetmix.com"
+##
+
+#--------------------------------------------------
+# Install Nginx if needed
+#--------------------------------------------------
+if [ $INSTALL_NGINX = "True" ]; then
+  echo -e "\n---- Installing and setting up Nginx ----"
+  sudo apt install nginx -y
+  cat <<EOF > ~/odoo
+  server {
+  listen 80;
+
+  # set proper server name after domain set
+  server_name $WEBSITE_NAME;
+
+  # Add Headers for odoo proxy mode
+  proxy_set_header X-Forwarded-Host \$host;
+  proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+  proxy_set_header X-Forwarded-Proto \$scheme;
+  proxy_set_header X-Real-IP \$remote_addr;
+  add_header X-Frame-Options "SAMEORIGIN";
+  add_header X-XSS-Protection "1; mode=block";
+  proxy_set_header X-Client-IP \$remote_addr;
+  proxy_set_header HTTP_X_FORWARDED_HOST \$remote_addr;
+
+  # SSL Settings
+  #ssl_session_cache shared:le_nginx_SSL:1m;
+  #ssl_session_timeout 1440m;
+  #ssl_protocols TLSv1.3 TLSv1.2;
+  #ssl_prefer_server_ciphers on;
+  #ssl_ecdh_curve secp521r1:secp384r1;
+  #ssl_ciphers EECDH+AESGCM:EECDH+AES256;
+
+ssl_stapling on;
+ssl_stapling_verify on;
+  #   odoo    log files
+  access_log  /var/log/nginx/$OE_USER-access.log;
+  error_log       /var/log/nginx/$OE_USER-error.log;
+
+  #   increase    proxy   buffer  size
+  proxy_buffers   16  64k;
+  proxy_buffer_size   128k;
+
+  proxy_read_timeout 900s;
+  proxy_connect_timeout 900s;
+  proxy_send_timeout 900s;
+
+  #   force   timeouts    if  the backend dies
+  proxy_next_upstream error   timeout invalid_header  http_500    http_502
+  http_503;
+
+  types {
+  text/less less;
+  text/scss scss;
+  }
+
+  #   enable  data    compression
+  gzip    on;
+  gzip_min_length 1100;
+  gzip_buffers    4   32k;
+  gzip_types  text/css text/less text/plain text/xml application/xml application/json application/javascript application/pdf image/jpeg image/png;
+  gzip_vary   on;
+  client_header_buffer_size 4k;
+  large_client_header_buffers 4 64k;
+  client_max_body_size 0;
+
+  location / {
+  proxy_pass    http://127.0.0.1:$OE_PORT;
+  # by default, do not forward anything
+  proxy_redirect off;
+  }
+
+  location /longpolling {
+  proxy_pass http://127.0.0.1:$LONGPOLLING_PORT;
+  }
+  location ~* .(js|css|png|jpg|jpeg|gif|ico)$ {
+  expires 2d;
+  proxy_pass http://127.0.0.1:$OE_PORT;
+  add_header Cache-Control "public, no-transform";
+  }
+  # cache some static data in memory for 60mins.
+  location ~ /[a-zA-Z0-9_-]*/static/ {
+  proxy_cache_valid 200 302 60m;
+  proxy_cache_valid 404      1m;
+  proxy_buffering    on;
+  expires 864000;
+  proxy_pass    http://127.0.0.1:$OE_PORT;
+  }
+  }
+EOF
+
+  sudo mv ~/odoo /etc/nginx/sites-available/
+  sudo ln -s /etc/nginx/sites-available/odoo /etc/nginx/sites-enabled/odoo
+  sudo rm /etc/nginx/sites-enabled/default
+  sudo service nginx reload
+
+  echo "Done! The Nginx server is up and running. Configuration can be found at /etc/nginx/sites-available/odoo"
+else
+  echo "Nginx isn't installed due to choice of the user!"
+fi
+
+#--------------------------------------------------
+# Enable ssl with certbot
+#--------------------------------------------------
+
+if [ $INSTALL_NGINX = "True" ] && [ $ENABLE_SSL = "True" ] && [ $ADMIN_EMAIL != "odoo@example.com" ]  && [ $WEBSITE_NAME != "_" ];then
+  sudo add-apt-repository ppa:certbot/certbot -y && sudo apt update -y
+  sudo apt install python3-certbot-nginx -y
+  sudo certbot --nginx -d $WEBSITE_NAME --noninteractive --agree-tos --email $ADMIN_EMAIL --redirect
+  sudo service nginx reload
+  echo "SSL/HTTPS is enabled!"
+else
+  echo "SSL/HTTPS isn't enabled due to choice of the user or because of a misconfiguration!"
+fi
+if [ $INSTALL_NGINX = "True" ]; then
+  echo "Nginx configuration file: /etc/nginx/sites-available/odoo"
+fi
+echo "-----------------------------------------------------------"
