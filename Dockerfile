90f1fee5e1e4035eaf3e0a18b9804321d0a9d25f
diga <gabbasov@itlibertas.com>
2022-10-01 00:14:56 +0500

    [NEW] cetmix_tower_file: Allow push and pull files from server
    [IMP] cetmix_tower_file: Get server version code (#16)
    
    * [IMP] cetmix_tower_file: Get server version code
    
    [FIX] ctemix_tower_file: fix cron errors
    
    [FIX] cetmix_tower_file: Fix render error
    
    [IMP] cetmix_tower_file:add note to template, and smart button to files
    
    [FIX] cetmix_tower_server:t2221 Fix connection error
    
    Update pre-commit
    
    [FIX] cetmix_tower_file: Fix helps, sanitize values

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,36 @@
-FROM cetmix/python3.7-ready4odoo:14.0
+FROM python:3.7.12-bullseye
+LABEL maintainer="Cetmix OU <docker@cetmix.com>"
+ARG ODOO_VERSION=14.0
+ARG DOCKER_ODOO_UID=9999
+ARG DOCKER_ODOO_GID=9999
+
+SHELL ["/bin/bash", "-xo", "pipefail", "-c"]
+
+# Generate locale C.UTF-8 for postgres and general locale data
+ENV LANG C.UTF-8
+
+# Install some deps, lessc and less-plugin-clean-css, and wkhtmltopdf
+RUN apt-get update && apt-get -y install node-less npm ssh libxml2-dev libxslt1-dev libldap2-dev\
+    libsasl2-dev libtiff5-dev libjpeg62-turbo-dev libopenjp2-7-dev\
+    zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev\
+    libharfbuzz-dev libfribidi-dev libxcb1-dev libpq-dev\
+    xvfb libfontconfig wkhtmltopdf python3-virtualenv virtualenv postgresql-client mc
+
+# Install rtlcss (on Debian buster)
+RUN npm install -g rtlcss
+
+# Enable VENV
+ENV VIRTUAL_ENV=/home/odoo/venv
+RUN python3.7 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+# Install debugger
+RUN python3.7 -m pip install --upgrade pip && \
+    python3.7 -m pip install debugpy
+
+# Create Virtual Environment and install dependencies
+RUN curl https://raw.githubusercontent.com/odoo/odoo/${ODOO_VERSION}/requirements.txt > /tmp/requirements.txt && \
+    pip3 install -r /tmp/requirements.txt
 
 # Install project related modules
 RUN python3.7 -m pip install paramiko scp
---
+++ cetmix_tower_file/models/cx_tower_file_template.py
@@ -0,0 +1,54 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class CxTowerFileTemplate(models.Model):
+    _name = "cx.tower.file.template"
+    _description = "Cx Tower File Template"
+
+    def _compute_file_count(self):
+        """
+        Compute total template files
+        """
+        for template in self:
+            template.file_count = len(template.file_ids)
+
+    name = fields.Char(help="Template Name")
+    file_name = fields.Char(
+        help="Default full file name with file type for example: test.txt",
+    )
+    code = fields.Text(string="File content")
+    server_dir = fields.Char(string="Directory on server")
+    file_ids = fields.One2many("cx.tower.file", "template_id")
+    file_count = fields.Integer(
+        "File(s)",
+        compute="_compute_file_count",
+    )
+    tag_ids = fields.Many2many(
+        comodel_name="cx.tower.tag",
+        relation="cx_tower_file_template_tag_rel",
+        column1="file_template_id",
+        column2="tag_id",
+        string="Tags",
+    )
+    note = fields.Text(help="This field is used to put some notes regarding template.")
+
+    def write(self, vals):
+        """
+        Override to update files related with the templates
+        """
+        result = super(CxTowerFileTemplate, self).write(vals)
+        for file in self.mapped("file_ids"):
+            file.write(file._get_file_values_from_related_template())
+        return result
+
+    def action_open_files(self):
+        """
+        Open current template files
+        """
+        action = self.env["ir.actions.actions"]._for_xml_id(
+            "cetmix_tower_file.cx_tower_file_action"
+        )
+        action["domain"] = [("id", "in", self.file_ids.ids)]
+        return action
