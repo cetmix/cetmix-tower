270b73e7ee1a3c9946061c630891db3ec1b52706
Dmytro Pavlov <dmitriy.paulov@gmail.com>
2024-04-12 09:37:35 +0300

    [IMP] cetmix_tower_server: implemented feature that allows to manage deletion policy for tower files being deleted from instance
    [REF] cetmix_tower_server: hide keep_when_deleted checkbox if source is not tower
    
    [FIX] cetmix_tower_server: additional check for file to server relation to avoid 'Expected singletone' error when server_id isn't set
    
    [FIX] cetmix_tower_server: catch file not found error when file is already deleted manually
    
    [REF] cetmix_tower_server: delete method refactoring
    
    [REF] cetmix_tower_server: renamed server action, made delete method call directly in unlink
    
    [REF] cetmix_tower_server: add help for keep_when_deleted, check access rights to unlink file
    
    [FIX] cetmix_tower_server: fix file unlink access check
    
    [ADD] cetmix_tower_server: add tests for file delete, refactor file tests
    
    [FIX] cetmix_tower_server: move access check to _process method, add access test
    
    [FIX] cetmix_tower_server: hide Delete from server menu for tower user group members

--- cetmix_tower_server/tests/test_file.py
+++ cetmix_tower_server/tests/test_file.py
@@ -1,14 +1,14 @@
 from unittest.mock import patch
 
+from odoo import exceptions
+
 from .common import TestTowerCommon
 
 
 class TestTowerCommand(TestTowerCommon):
-    def test_upload_file(self):
-        """
-        Upload file from tower to server
-        """
-        file_template = self.env["cx.tower.file.template"].create(
+    def setUp(self):
+        super().setUp()
+        self.file_template = self.env["cx.tower.file.template"].create(
             {
                 "name": "Test",
                 "file_name": "test.txt",
@@ -16,29 +16,14 @@ class TestTowerCommand(TestTowerCommon):
                 "code": "Hello, world!",
             }
         )
-        file = self.env["cx.tower.file"].create(
+        self.file = self.env["cx.tower.file"].create(
             {
                 "source": "tower",
-                "template_id": file_template.id,
+                "template_id": self.file_template.id,
                 "server_id": self.server_test_1.id,
             }
         )
-
-        cx_tower_server_obj = self.registry["cx.tower.server"]
-
-        def upload_file(this, file, remote_path):
-            if file == "Hello, world!" and remote_path == "/var/tmp":
-                return "ok"
-
-        with patch.object(cx_tower_server_obj, "upload_file", upload_file):
-            file.action_push_to_server()
-            self.assertEqual(file.server_response, "ok")
-
-    def test_download_file(self):
-        """
-        Download file from server to tower
-        """
-        file = self.env["cx.tower.file"].create(
+        self.file_2 = self.env["cx.tower.file"].create(
             {
                 "name": "test.txt",
                 "source": "server",
@@ -47,36 +32,10 @@ class TestTowerCommand(TestTowerCommon):
             }
         )
 
-        def download_file(this, remote_path):
-            if remote_path == "/var/tmp/test.txt":
-                return "Hello, world!"
-
-        cx_tower_server_obj = self.registry["cx.tower.server"]
-
-        with patch.object(cx_tower_server_obj, "download_file", download_file):
-            file.action_pull_from_server()
-            self.assertEqual(file.code, "Hello, world!")
-
-    def test_get_current_server_code(self):
+    def test_upload_file(self):
         """
-        Download file from server to tower
+        Upload file from tower to server
         """
-        file_template = self.env["cx.tower.file.template"].create(
-            {
-                "name": "Test",
-                "file_name": "test.txt",
-                "server_dir": "/var/tmp",
-                "code": "Hello, world!",
-            }
-        )
-        file = self.env["cx.tower.file"].create(
-            {
-                "source": "tower",
-                "template_id": file_template.id,
-                "server_id": self.server_test_1.id,
-            }
-        )
-
         cx_tower_server_obj = self.registry["cx.tower.server"]
 
         def upload_file(this, file, remote_path):
@@ -84,51 +43,87 @@ class TestTowerCommand(TestTowerCommon):
                 return "ok"
 
         with patch.object(cx_tower_server_obj, "upload_file", upload_file):
-            file.action_push_to_server()
-            self.assertEqual(file.server_response, "ok")
+            self.file.action_push_to_server()
+            self.assertEqual(self.file.server_response, "ok")
+
+    def test_delete_file(self):
+        """
+        Delete file remotely from server
+        """
+        cx_tower_server_obj = self.registry["cx.tower.server"]
+
+        def delete_file(this, remote_path):
+            if remote_path == "/var/tmp":
+                return "ok"
+
+        with patch.object(cx_tower_server_obj, "delete_file", delete_file):
+            self.file.action_delete_from_server()
+            self.assertEqual(self.file.server_response, "ok")
+
+    def test_delete_file_access(self):
+        """
+        Test delete file access
+        """
+        with self.assertRaises(exceptions.AccessError):
+            self.file.with_user(self.user_bob).delete(raise_error=True)
+
+    def test_download_file(self):
+        """
+        Download file from server to tower
+        """
+
+        def download_file(this, remote_path):
+            if remote_path == "/var/tmp/test.txt":
+                return "Hello, world!"
+
+        cx_tower_server_obj = self.registry["cx.tower.server"]
+
+        with patch.object(cx_tower_server_obj, "download_file", download_file):
+            self.file_2.action_pull_from_server()
+            self.assertEqual(self.file_2.code, "Hello, world!")
+
+    def test_get_current_server_code(self):
+        """
+        Download file from server to tower
+        """
+        cx_tower_server_obj = self.registry["cx.tower.server"]
+
+        def upload_file(this, file, remote_path):
+            if file == "Hello, world!" and remote_path == "/var/tmp":
+                return "ok"
+
+        with patch.object(cx_tower_server_obj, "upload_file", upload_file):
+            self.file.action_push_to_server()
+            self.assertEqual(self.file.server_response, "ok")
 
         def download_file(this, remote_path):
             if remote_path == "/var/tmp/test.txt":
                 return "Hello, world!"
 
         with patch.object(cx_tower_server_obj, "download_file", download_file):
-            file.action_get_current_server_code()
-            self.assertEqual(file.code_on_server, "Hello, world!")
+            self.file.action_get_current_server_code()
+            self.assertEqual(self.file.code_on_server, "Hello, world!")
 
     def test_modify_template_code(self):
         code = "Pepe frog is happy as always"
-        file_template = self.env["cx.tower.file.template"].create(
-            {
-                "name": "Test",
-                "file_name": "test.txt",
-                "server_dir": "/var/tmp",
-                "code": code,
-            }
-        )
-        file = self.env["cx.tower.file"].create(
-            {
-                "source": "tower",
-                "template_id": file_template.id,
-                "server_id": self.server_test_1.id,
-            }
-        )
+        self.file_template.code = code
 
         # Check file code before modifications
         self.assertTrue(
-            file.code == code,
+            self.file.code == code,
             msg="File code should be the same as template before any modifications",
         )
 
         # Make possible to modify file code
-        file.action_modify_code()
+        self.file.action_modify_code()
 
         # Check if template was removed from file
         self.assertFalse(
-            file.template_id,
+            self.file.template_id,
             msg="File template should be removed after modifying code.",
         )
 
         # Check if file code remains the same
         self.assertTrue(
-            file.code == code, msg="File code should be the same as template."
+            self.file.code == code, msg="File code should be the same as template."
         )
