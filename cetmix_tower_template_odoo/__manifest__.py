d742974e38a8449e4d9ae62825b3f76dc39f332e
George Smirnov <sombunall@gmail.com>
2024-04-25 02:15:57 +0200

    [IMP] cetmix_tower_template_odoo: add variable data file

--- cetmix_tower_template_odoo/__manifest__.py
+++ cetmix_tower_template_odoo/__manifest__.py
@@ -2,17 +2,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cetmix Tower Template Odoo',
-    'description': """
+    "name": "Cetmix Tower Template Odoo",
+    "summary": """
         Cetmix Tower Odoo deployment template""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Cetmix',
-    'website': 'https://cetmix.com',
-    'depends': ['cetmix_tower_server'],
-    
-    'data': [ "data/cx_tower_variable_data.xml",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Cetmix",
+    "website": "https://cetmix.com",
+    "depends": ["cetmix_tower_server"],
+    "data": [
+        "data/cx_tower_variable_data.xml",
+        "data/cx_tower_variable_value_data.xml",
+        "data/cx_tower_command_data.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
