82100823318a6867007ad46c357bd90033347788
Henrik Norlin <henrik.norl@gmail.com>
2024-06-19 20:25:16 +0200

    [ADD] cetmix_tower_data_doodba

---
+++ cetmix_tower_data_doodba/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Cetmix OU
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cetmix Tower Data - Doodba",
+    "summary": "Cetmix Tower standard Doodba deployment",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Loym",
+    "website": "https://cetmix.com",
+    "depends": ["cetmix_tower_data_odoo"],
+    "data": [
+        "data/cx_tower_tag_data.xml",
+        "data/commands.xml",
+        "data/variables.xml",
+        "data/servers.xml",
+    ],
+    "demo": [],
+}
