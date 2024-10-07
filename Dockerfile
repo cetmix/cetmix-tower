86cc957af2cfeddaabcdf5a128f42709565dd638
Ivan Sokolov <jzx115@gmail.com>
2024-10-07 23:17:22 +0200

    [ADD] cetmix_tower_server_yaml Tower YAML support
    Special mixin to be inherited for YAML support.
    cx.tower.command: generate YAML code based on command properties.
    cx.tower.command: Compose command from YAML

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM my-odoo:14.0
 
 # Install project related modules as root gi
 USER root
-RUN python3.10 -m pip install paramiko
+RUN python3.10 -m pip install paramiko pyyaml
 USER odoo
---
+++ cetmix_tower_server_yaml/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright Cetmix OU
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Cetmix Tower Server YAML",
+    "summary": "Cetmix Tower YAML export/import",
+    "version": "14.0.1.0.0",
+    "category": "Productivity",
+    "website": "https://cetmix.com",
+    "author": "Cetmix",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["cetmix_tower_server"],
+    "external_dependencies": {"python": ["pyyaml"]},
+    "data": [
+        "views/cx_tower_command_view.xml",
+    ],
+}
