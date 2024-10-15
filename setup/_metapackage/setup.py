15c9ab6708f00f967010ef78c92e48e6e2c78252
CetmixGitBot <gitbot@cetmix.com>
2024-10-15 19:36:06 +0000

    [BOT] post-merge updates

---
+++ setup/_metapackage/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-cetmix-cetmix-tower",
+    description="Meta package for cetmix-cetmix-tower Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-cetmix_tower_server',
+        'odoo14-addon-cetmix_tower_server_queue',
+        'odoo14-addon-cetmix_tower_yaml',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+        'Framework :: Odoo :: 14.0',
+    ]
+)
