b056f3fd8973fea045588b03aed583dbc8e267cc
Ivan Sokolov <git-email-for-ivan@cetmix.com>
2024-11-04 22:25:15 +0100

    [IMP] cetmix_tower_server: Server Template mods
    Improve UX by adding new features and modifying existing ones.
    
    Server template and wizard
    --------------------------
    
    - Variable values can be edited in wizard before server creation
    - Make some template fields not required because they can be set later
    - Add 'Create Server' action in the kanban tile 'Action' menu
    - Improve server template wizard layout
    
    Server
    ------
    
    Add context key that allows to skip constraint checks for ssh fields.
    This is needed in order to avoid exceptions when creating a new server
    from a server template using wizard.
    
    While it's possible to modify server properties from a flight plan that
    is called after server is created those SSH fields can be populated
    later. So there is no need to block them.
    
    Group several constraint violation messages into one to deliver
    all notifications at once.
    
    Task: 4027

--- cetmix_tower_server/wizards/cx_tower_server_template_create_wizard.py
+++ cetmix_tower_server/wizards/cx_tower_server_template_create_wizard.py
@@ -5,6 +5,8 @@ from odoo import fields, models
 
 
 class CxTowerServerTemplateCreateWizard(models.TransientModel):
+    """Create new server from template"""
+
     _name = "cx.tower.server.template.create.wizard"
     _description = "Create new server from template"
 
@@ -17,10 +19,16 @@ class CxTowerServerTemplateCreateWizard(models.TransientModel):
         string="Server Name",
         required=True,
     )
+    color = fields.Integer(help="For better visualization in views")
     ip_v4_address = fields.Char(string="IPv4 Address")
     ip_v6_address = fields.Char(string="IPv6 Address")
-    ssh_port = fields.Char(string="SSH port", required=True, default="22")
-    ssh_username = fields.Char(string="SSH Username", required=True)
+    ssh_port = fields.Char(string="SSH port", default="22")
+    ssh_username = fields.Char(
+        string="SSH Username",
+        required=True,
+        help="This is required, however you can change this later "
+        "in the server settings",
+    )
     ssh_password = fields.Char(string="SSH Password")
     ssh_key_id = fields.Many2one(
         comodel_name="cx.tower.key",
@@ -36,22 +44,45 @@ class CxTowerServerTemplateCreateWizard(models.TransientModel):
         default="p",
         required=True,
     )
+    line_ids = fields.One2many(
+        comodel_name="cx.tower.server.template.create.wizard.line",
+        inverse_name="wizard_id",
+    )
+
+    def _prepare_server_parameters(self):
+        """Prepare new server parameters
+
+        Returns:
+            dict(): New server parameters
+        """
+        res = {
+            "ip_v4_address": self.ip_v4_address,
+            "ip_v6_address": self.ip_v6_address,
+            "ssh_port": self.ssh_port,
+            "ssh_username": self.ssh_username,
+            "ssh_password": self.ssh_password,
+            "ssh_key_id": self.ssh_key_id.id,
+            "ssh_auth_mode": self.ssh_auth_mode,
+        }
+        if self.line_ids:
+            res.update(
+                {
+                    "configuration_variables": {
+                        line.variable_reference: line.value_char
+                        for line in self.line_ids
+                    }
+                }
+            )
+        return res
 
     def action_confirm(self):
         """
         Create and open new created server from template
         """
         self.ensure_one()
-        server = self.server_template_id._create_new_server(
-            self.name,
-            ip_v4_address=self.ip_v4_address,
-            ip_v6_address=self.ip_v6_address,
-            ssh_port=self.ssh_port,
-            ssh_username=self.ssh_username,
-            ssh_password=self.ssh_password,
-            ssh_key_id=self.ssh_key_id.id,
-            ssh_auth_mode=self.ssh_auth_mode,
-        )
+
+        kwargs = self._prepare_server_parameters()
+        server = self.server_template_id._create_new_server(self.name, **kwargs)
         action = self.env["ir.actions.actions"]._for_xml_id(
             "cetmix_tower_server.action_cx_tower_server"
         )
@@ -59,3 +90,15 @@ class CxTowerServerTemplateCreateWizard(models.TransientModel):
             {"view_mode": "form", "res_id": server.id, "views": [(False, "form")]}
         )
         return action
+
+
+class CxTowerServerTemplateCreateWizardVariableLine(models.TransientModel):
+    """Configuration variables"""
+
+    _name = "cx.tower.server.template.create.wizard.line"
+    _description = "Create new server from template variables"
+
+    wizard_id = fields.Many2one("cx.tower.server.template.create.wizard")
+    variable_id = fields.Many2one(comodel_name="cx.tower.variable", required=True)
+    variable_reference = fields.Char(related="variable_id.reference", readonly=True)
+    value_char = fields.Char(string="Value")
