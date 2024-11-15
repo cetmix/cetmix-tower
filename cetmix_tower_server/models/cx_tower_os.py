5a8c0312f9836adb61079deb2ba987e407c2929c
dessanhemrayev <dessanhemrayev@gmail.com>
2024-11-15 21:39:30 +0300

    [IMP] cetmix_tower_server: References for OS
    This commit adds references to OSs for easier export/import and action referencing.
    
    Changes
    
     - Inherit from cx.tower.reference.mixin
     - Remove extra field
     - Add reference fields to OS views, including search view
     - Update documentation to reflect new reference fields in OSs
    
    Task: 4044

--- cetmix_tower_server/models/cx_tower_os.py
+++ cetmix_tower_server/models/cx_tower_os.py
@@ -5,8 +5,10 @@ from odoo import fields, models
 
 class CxTowerOs(models.Model):
     _name = "cx.tower.os"
+    _inherit = [
+        "cx.tower.reference.mixin",
+    ]
     _description = "Cetmix Tower Operating System"
 
-    name = fields.Char(required=True)
     color = fields.Integer(help="For better visualization in views")
     parent_id = fields.Many2one(string="Previous Version", comodel_name="cx.tower.os")
