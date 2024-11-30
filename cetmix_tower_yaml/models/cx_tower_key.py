74498b93eb6a1c8b5ca6468b9d20a631142c8d1a
Ivan Sokolov <git-email-for-ivan@cetmix.com>
2024-11-30 22:06:34 +0100

    [IMP] cetmix_tower_yaml: Secret value check
    When exporting a key the secret value is replaced with the mask.
    This is done to prevent leaking sensitive data during export.
    However this may create an issue when the same file is imported back.
    Because if this mask still remains in the imported file
    it will overwrite the actual secret value.
    
    This commit prevents this from happening by checking the secret value
    before creating or updating a key.
    If the mask is still present in the file as a value an error is raised.
    
    Task: 4119

--- cetmix_tower_yaml/models/cx_tower_key.py
+++ cetmix_tower_yaml/models/cx_tower_key.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2024 Cetmix OÜ
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class CxTowerKey(models.Model):
@@ -11,6 +12,20 @@ class CxTowerKey(models.Model):
     ]
 
     SECRET_VALUE_MASK = "********"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        # Check secret value before creating a record
+        for vals in vals_list:
+            if "secret_value" in vals:
+                self._check_secret_value(vals["secret_value"])
+        return super().create(vals)
+
+    def write(self, vals):
+        # Check secret value before updating a record
+        if "secret_value" in vals:
+            self._check_secret_value(vals["secret_value"])
+        return super().write(vals)
 
     def _get_fields_for_yaml(self):
         res = super()._get_fields_for_yaml()
@@ -29,3 +44,24 @@ class CxTowerKey(models.Model):
         if res.get("secret_value"):
             res["secret_value"] = self.SECRET_VALUE_MASK
         return res
+
+    def _check_secret_value(self, secret_value):
+        """Check secret value before creating or updating a record
+        We are not using a constraint because we need to check
+        the value before creating or updating a record in the database.
+
+        Args:
+            secret_value (Char): secret value to check
+
+        Raises:
+            ValidationError: If secret value fails the check
+        """
+
+        # Prevent saving secret mask as a value
+        if secret_value == self.SECRET_VALUE_MASK:
+            raise ValidationError(
+                _(
+                    "Value '%(val)s' is used as default secret mask and cannot be set as a secret value",  # noqa: E501
+                    val=self.SECRET_VALUE_MASK,
+                )
+            )
