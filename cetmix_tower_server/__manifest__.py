9b9a839b33f1757ab214e33cd4b665cc49ef91bf
Dmytro Meita <mejta.diman@gmail.com>
2024-12-19 02:26:20 +0200

    [IMP] cetmix_tower_server: Centralize and enhance variable management logic
    Centralized and streamlined the logic for managing `variable_ids` in `CxTowerTemplateMixin` and related models (`CxTowerCommand`, `CxTowerFile`, and `CxTowerFileTemplate`):
    
    - Introduced `_get_relation_params` in `CxTowerTemplateMixin` to dynamically extract `relation`, `column1`, and `column2` for `variable_ids` based on field metadata.
    - Created `_get_depends_fields` as a customizable method to define dependent fields for each model:
      - `CxTowerCommand` uses ["code", "path"].
      - `CxTowerFile` uses ["code", "server_dir", "name"].
      - `CxTowerFileTemplate` uses ["code", "server_dir", "file_name"].
    - Enhanced `_compute_variable_ids` in `CxTowerTemplateMixin` to dynamically compute `variable_ids` using the dependencies provided by `_get_depends_fields`.
    - Updated model definitions (`CxTowerCommand`, `CxTowerFile`, and `CxTowerFileTemplate`) to:
      - Retain unique relation details (`relation`, `column1`, and `column2`) for `variable_ids`.
      - Delegate computation logic to the mixin for better maintainability.
    - Added professional docstrings for all methods, ensuring clear documentation and adherence to professional standards.
    
    Additional changes:
    - Adjusted UI views to include a read-only field `variable_ids`:
      - Visible only to users in the `Manager` and `Root` groups.
    
    Task ID: 4167

--- cetmix_tower_server/__manifest__.py
+++ cetmix_tower_server/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower Server Management",
     "summary": "Flexible Server Management directly from Odoo",
-    "version": "14.0.0.4.2",
+    "version": "14.0.0.4.3",
     "category": "Productivity",
     "website": "https://cetmix.com",
     "author": "Cetmix",
