c808e52315eb0af62ed2242aefebe4580e90ec5f
Ivan Sokolov <git-email-for-ivan@cetmix.com>
2025-01-06 23:28:26 +0100

    [FIX] cetmix_tower_yaml: warnings in tests
    Add model description to the wizard.
    Absent description in the wizard causes warnings in tests.

--- cetmix_tower_yaml/wizards/cx_tower_yaml_import_wiz.py
+++ cetmix_tower_yaml/wizards/cx_tower_yaml_import_wiz.py
@@ -5,6 +5,7 @@ from odoo import api, fields, models
 
 class CxTowerYamlImportWiz(models.TransientModel):
     _name = "cx.tower.yaml.import.wiz"
+    _description = "Cetmix Tower YAML Import Wizard"
     """
     Process YAML data and create records in Odoo.
     """
