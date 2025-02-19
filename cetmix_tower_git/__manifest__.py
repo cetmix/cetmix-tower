0d0bb0c0fe16a376e12a3f88bcfb7d03345db6da
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-02-19 21:33:56 +0100

    [IMP] cetmix_tower_git: New access rules
    New access rules according to the new access rules for the server.

--- cetmix_tower_git/__manifest__.py
+++ cetmix_tower_git/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower Git",
     "summary": "Cetmix Tower Git Management Tools",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Productivity",
     "website": "https://cetmix.com",
     "author": "Cetmix",
