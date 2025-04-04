c39a72262cd3232608b5a661c95237b2aae5e550
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-04-04 13:31:53 +0200

    [IMP] cetmix_tower_server: command timeout
    Check 'zombie' commands and mark them as finished.
    This is needed to avoid infinite loops when a command is stuck.
    
    Task: 4509

--- cetmix_tower_server/models/res_config_settings.py
+++ cetmix_tower_server/models/res_config_settings.py
@@ -1,4 +1,4 @@
-from odoo import _, models
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -9,7 +9,14 @@ class ResConfigSettings(models.TransientModel):
 
     _inherit = "res.config.settings"
 
-    def action_configure_cron(self):
+    cetmix_tower_command_timeout = fields.Integer(
+        string="Command Timeout",
+        config_parameter="cetmix_tower_server.command_timeout",
+        help="Timeout for commands in seconds after which"
+        " the command will be terminated",
+    )
+
+    def action_configure_cron_pull_files_from_server(self):
         """
         Configure cron job to pull files from server
         """
@@ -20,7 +27,24 @@ class ResConfigSettings(models.TransientModel):
         if not cron_id:
             raise ValidationError(_("Cron job not found"))
         return {
-            "name": _("Pull Files from Server"),
+            "name": _("Cron Job"),
+            "views": [(False, "form")],
+            "res_model": "ir.cron",
+            "res_id": cron_id,
+            "type": "ir.actions.act_window",
+            "target": "new",
+        }
+
+    def action_configure_zombie_commands_cron(self):
+        """
+        Configure cron job to check zombie commands
+        """
+        self.ensure_one()
+        cron_id = self.env.ref("cetmix_tower_server.ir_cron_check_zombie_commands").id
+        if not cron_id:
+            raise ValidationError(_("Cron job not found"))
+        return {
+            "name": _("Cron Job"),
             "views": [(False, "form")],
             "res_model": "ir.cron",
             "res_id": cron_id,
