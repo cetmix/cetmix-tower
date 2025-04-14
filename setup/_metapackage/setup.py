b65cf31603b41a8df0aa6a0f9ec6e9e3012c9615
CetmixGitBot <gitbot@cetmix.com>
2025-04-14 08:31:15 +0000

    [BOT] post-merge updates

--- setup/_metapackage/setup.py
+++ setup/_metapackage/setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     install_requires=[
         'odoo-addon-cetmix_tower_git>=16.0dev,<16.1dev',
         'odoo-addon-cetmix_tower_server>=16.0dev,<16.1dev',
+        'odoo-addon-cetmix_tower_server_notify_backend>=16.0dev,<16.1dev',
         'odoo-addon-cetmix_tower_yaml>=16.0dev,<16.1dev',
     ],
     classifiers=[
