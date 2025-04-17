5e1a63abbbb51ca9ef47d32a2ff203fdfe4e69c6
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-04-17 22:22:17 +0200

    [MIG] cetmix_tower_server_queue: 16.0
    Start migration to Odoo 16.0
    
    Task: 4595

--- cetmix_tower_server_queue/__manifest__.py
+++ cetmix_tower_server_queue/__manifest__.py
@@ -3,8 +3,8 @@
 {
     "name": "Cetmix Tower Server Queue",
     "summary": "Cetmix Tower asynchronous task execution using 'queue_job'",
-    "version": "14.0.1.0.5",
-    "development_status": "Production/Stable",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
     "category": "Productivity",
     "website": "https://cetmix.com",
     "author": "Cetmix",
--- cetmix_tower_server_queue/readme/HISTORY.md
+++ cetmix_tower_server_queue/readme/HISTORY.md
@@ -1,3 +1,3 @@
-## 14.0.1.0.5 (2025-04-17)
+## 16.0.1.0.0
 
-- Features: Cancel the related queue job when a command is forcefully stopped. (4550)
+Release for Odoo 16.0
