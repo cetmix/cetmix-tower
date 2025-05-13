eecdda0a79155285b29065fa1db2897b2c2e9543
Mikhail Lapin <lapin.mike@yyahoo.com>
2025-05-13 20:29:10 +0200

    [IMP] cetmix_tower_aws: banner and description add
    Add the banner and live test url in manifest
    
    Task:4677

--- cetmix_tower_aws/models/constants.py
+++ cetmix_tower_aws/models/constants.py
@@ -14,7 +14,11 @@ BOTO3_HELP_TEXT = _(
 
 # HTML-formatted version of boto3 help text for command help display
 BOTO3_HELP_TEXT_HTML = _(
+    "<ul>"
     "<li><code>boto3</code>: Python 'boto3' library for AWS services. "
     "Available methods: 'client', 'resource', 'Session'<br/>"
-    "Supports AWS services like EC2, S3, RDS, Lambda, CloudWatch, etc.</li>"
+    "Supports AWS services like EC2, S3, RDS, Lambda, CloudWatch, etc.<br/>"
+    "Please check the <a href='https://boto3.amazonaws.com/v1/documentation/api/latest/index.html' target='_blank'>Boto3 Documentation</a> for the detailed information about the services and methods."  # noqa: E501
+    "</li>"
+    "</ul>"
 )
