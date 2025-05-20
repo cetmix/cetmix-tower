8330ab18acd80d67f879936648ddf53a618c1d60
Giovanni <giovanni@gslab.it>
2025-05-20 18:52:40 +0200

    [ADD] cetmix_tower_ovh: add module This module integrates the OVH SDK into Cetmix Tower, allowing users to manage and automate a wide range of OVH services.
    It extends the `cx.tower.command` template to provide access to the OVH Python library, allowing OVH commands to be executed directly from the Cetmix Tower interface.
    
    Key Features:
    - Secure OVH library integration for full interaction with OVH services
    - Extends the command evaluation context with OVH functionality
    - Requires the Python dependency `ovh`

---
+++ cetmix_tower_ovh/models/cx_tower_command.py
@@ -0,0 +1,56 @@
+# Copyright 2024 Cetmix OÜ
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+from odoo.tools.safe_eval import wrap_module
+
+from . import constants
+
+# Wrap ovh safely
+ovh = wrap_module(__import__("ovh"), ["Client"])
+tldextract = wrap_module(__import__("tldextract"), ["extract"])
+
+
+class CxTowerCommand(models.Model):
+    """Extends cx.tower.command to add OVH functionality."""
+
+    _inherit = "cx.tower.command"
+
+    @api.model
+    def _get_eval_context(self, server=None):
+        """
+        Overrides _get_eval_context to add OVH to the evaluation context.
+        """
+        eval_context = super()._get_eval_context(server=server)
+        eval_context.update({"ovh": ovh, "tldextract": tldextract})
+        return eval_context
+
+    def _compute_code(self):
+        """Compute default code with OVH information.
+
+        Extends the original method to add OVH documentation
+        to Python code commands.
+        """
+        # First let core populate code
+        super()._compute_code()
+
+        for rec in self:
+            if rec.action == "python_code" and rec.code:
+                formatted_code = rec.code.rstrip()
+                # Add OVH client usage examples and documentation
+                help_code = f"{formatted_code}\n{constants.OVH_HELP_TEXT}"
+                rec.code = help_code
+
+    def _compute_command_help(self):
+        """Compute command help with OVH information.
+
+        Extends the original method to add OVH documentation
+        to Python code command help.
+        """
+        # First let core populate command help
+        super()._compute_command_help()
+
+        for rec in self:
+            if rec.action == "python_code" and rec.command_help:
+                help_text = f"{rec.command_help}{constants.OVH_HELP_TEXT_HTML}"
+                rec.command_help = help_text
