4ac8bfa9032f52ec0c117d643b7ceec01cb57d2c
Viktoriia <kuramatw@gmail.com>
2025-06-03 22:53:22 +0200

    [IMP] cetmix_tower_server_queue: copy job error to command log
    Task: 4748

--- cetmix_tower_server_queue/models/queue_job.py
+++ cetmix_tower_server_queue/models/queue_job.py
@@ -9,11 +9,15 @@ class QueueJob(models.Model):
     QUEUE_JOB_ERROR = 601
 
     def write(self, vals):
-        """Override write method to update command status in the log record"""
+        """
+        Override write method to update command status
+        and write error information in the log record
+        """
         if vals.get("state") == "failed":
             log_record = self.kwargs.get("log_record")
             if log_record:
                 log_record.finish(
                     status=self.QUEUE_JOB_ERROR,
+                    error=vals.get("exc_info"),
                 )
         return super(QueueJob, self).write(vals)
