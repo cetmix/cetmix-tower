f7b2317d2e09d7695c826bb477ef60a115f068a8
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-06-09 17:04:09 +0200

    [REF] cetmix_tower_ovh: Library import refactor
    Refactor the library import using the latest updates in the `cetmix_tower_server` module.
    Use just one function instead of three.
    
    Task: 4768

--- cetmix_tower_ovh/models/cx_tower_command.py
+++ cetmix_tower_ovh/models/cx_tower_command.py
@@ -1,10 +1,9 @@
 # Copyright 2024 Cetmix OÜ
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import _, models
+from odoo.tools import ormcache
 from odoo.tools.safe_eval import wrap_module
-
-from . import constants
 
 # Wrap ovh safely
 ovh = wrap_module(__import__("ovh"), ["Client"])
@@ -15,41 +14,26 @@ class CxTowerCommand(models.Model):
 
     _inherit = "cx.tower.command"
 
-    @api.model
-    def _get_eval_context(self, server=None):
+    @ormcache()  # Use ormcache to cache the result
+    def _get_python_command_libraries(self):
         """
-        Overrides _get_eval_context to add OVH to the evaluation context.
+        Add the ovh library to the available libraries.
         """
-        eval_context = super()._get_eval_context(server=server)
-        eval_context.update({"ovh": ovh})
-        return eval_context
-
-    def _compute_code(self):
-        """Compute default code with OVH information.
-
-        Extends the original method to add OVH documentation
-        to Python code commands.
-        """
-        # First let core populate code
-        super()._compute_code()
-
-        for rec in self:
-            if rec.action == "python_code" and rec.code:
-                formatted_code = rec.code.rstrip()
-                # Add OVH client usage examples and documentation
-                help_code = f"{formatted_code}\n{constants.OVH_HELP_TEXT}"
-                rec.code = help_code
-
-    def _compute_command_help(self):
-        """Compute command help with OVH information.
-
-        Extends the original method to add OVH documentation
-        to Python code command help.
-        """
-        # First let core populate command help
-        super()._compute_command_help()
-
-        for rec in self:
-            if rec.action == "python_code" and rec.command_help:
-                help_text = f"{rec.command_help}{constants.OVH_HELP_TEXT_HTML}"
-                rec.command_help = help_text
+        python_libraries = super()._get_python_command_libraries()
+        python_libraries.update(
+            {
+                "ovh": {
+                    "import": ovh,
+                    "help": _(
+                        "Python 'ovh' library for OVH services. "
+                        "Available methods: 'Client'<br/>"
+                        "Supports OVH services<br/>"
+                        "Please check the <a "
+                        "href='https://eu.api.ovh.com/console/?section=%2FallDom&branch=v1'"
+                        " target='_blank'>OVH Documentation</a> for detailed "
+                        "information about the services and methods."
+                    ),
+                }
+            }
+        )
+        return python_libraries
