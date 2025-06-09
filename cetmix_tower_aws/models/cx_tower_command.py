a19b89fd4c0fa7faca0bbe2155ec2658d3031d00
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-06-09 16:52:57 +0200

    [REF] cetmix_tower_aws: Library import refactor
    Refactor the library import using the latest updates in the `cetmix_tower_server` module.
    Use just one function instead of three.
    
    Task: 4768

--- cetmix_tower_aws/models/cx_tower_command.py
+++ cetmix_tower_aws/models/cx_tower_command.py
@@ -1,10 +1,9 @@
 # Copyright 2024 Cetmix OÜ
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import _, models
+from odoo.tools import ormcache
 from odoo.tools.safe_eval import wrap_module
-
-from . import constants
 
 # Wrap boto3 safely
 boto3 = wrap_module(__import__("boto3"), ["client", "resource", "Session"])
@@ -15,40 +14,27 @@ class CxTowerCommand(models.Model):
 
     _inherit = "cx.tower.command"
 
-    @api.model
-    def _get_eval_context(self, server=None):
+    @ormcache()  # Use ormcache to cache the result
+    def _get_python_command_libraries(self):
         """
-        Overrides _get_eval_context to add boto3 to the evaluation context.
+        Add the boto3 library to the available libraries.
         """
-        eval_context = super()._get_eval_context(server=server)
-        eval_context["boto3"] = boto3
-        return eval_context
-
-    def _compute_code(self):
-        """Compute default code with boto3 information.
-
-        Extends the original method to add boto3 documentation
-        to Python code commands.
-        """
-        # First let core populate code
-        super()._compute_code()
-
-        for rec in self:
-            if rec.action == "python_code" and rec.code:
-                formatted_code = rec.code.rstrip()
-                # Append the boto3 help text
-                rec.code = f"{formatted_code}\n{constants.BOTO3_HELP_TEXT}"
-
-    def _compute_command_help(self):
-        """Compute command help with boto3 information.
-
-        Extends the original method to add boto3 documentation
-        to Python code command help.
-        """
-        # First let core populate command help
-        super()._compute_command_help()
-
-        for rec in self:
-            if rec.action == "python_code" and rec.command_help:
-                help_text = f"{rec.command_help}{constants.BOTO3_HELP_TEXT_HTML}"
-                rec.command_help = help_text
+        python_libraries = super()._get_python_command_libraries()
+        python_libraries.update(
+            {
+                "boto3": {
+                    "import": boto3,
+                    "help": _(
+                        "Python 'boto3' library for AWS services. "
+                        "Available methods: 'client', 'resource', 'Session'<br/>"
+                        "Supports AWS services like EC2, S3, RDS, Lambda, "
+                        "CloudWatch, etc.<br/>"
+                        "Please check the <a "
+                        "href='https://boto3.amazonaws.com/v1/documentation/api/latest/index.html'"
+                        " target='_blank'>Boto3 Documentation</a> for the detailed "
+                        "information about the services and methods."
+                    ),
+                },
+            }
+        )
+        return python_libraries
