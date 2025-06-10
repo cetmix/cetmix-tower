16febb8c99dae6a86ae0ef4d0f7b9f2d487e23e5
CetmixGitBot <gitbot@cetmix.com>
2025-06-10 15:48:19 +0000

    [BOT] post-merge updates

--- cetmix_tower_server/__manifest__.py
+++ cetmix_tower_server/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower Server",
     "summary": "Manage servers and applications from Odoo",
-    "version": "16.0.1.0.12",
+    "version": "16.0.1.0.13",
     "development_status": "Beta",
     "category": "Productivity",
     "website": "https://cetmix.com",
