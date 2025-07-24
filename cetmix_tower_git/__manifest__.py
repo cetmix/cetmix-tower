1d2b86b338de67f3b837b96efaa6702ba3010a7b
Mohajiro <lavrovuriy@gmail.com>
2025-07-24 11:40:24 +0200

    [IMP] cetmix_tower_yaml: export server’s command_ids and plan_ids
    Include M2M fields for commands and flight plans in the YAML export
    of cx.tower.server so that related records are automatically exported
    and restored during import.
    Add tests for testing all functionallity
    
    Task: 4849

--- cetmix_tower_git/__manifest__.py
+++ cetmix_tower_git/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower Git",
     "summary": "Cetmix Tower Git Management Tools",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "development_status": "Beta",
     "category": "Productivity",
     "website": "https://cetmix.com",
--- cetmix_tower_git/readme/HISTORY.md
+++ cetmix_tower_git/readme/HISTORY.md
@@ -1,3 +1,8 @@
+## 17.0.1.0.3 (2025-07-30)
+
+- Features: Export related commands and flight plans together with server (4849)
+
+
 ## 17.0.1.0.2 (2025-07-03)
 
 - Bugfixes: Duplicated file is created when importing a YAML file with a git project. (4715)
