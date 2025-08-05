f6067c983ecaccf274219346d07367319cbe71ca
CetmixGitBot <gitbot@cetmix.com>
2025-08-05 13:13:58 +0000

    [BOT] post-merge updates

--- cetmix_tower_server/__manifest__.py
+++ cetmix_tower_server/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower Server",
     "summary": "Manage servers and applications from Odoo",
-    "version": "14.0.1.6.0",
+    "version": "14.0.1.6.1",
     "development_status": "Production/Stable",
     "category": "Productivity",
     "website": "https://cetmix.com",
--- cetmix_tower_server/readme/HISTORY.md
+++ cetmix_tower_server/readme/HISTORY.md
@@ -1,3 +1,8 @@
+## 14.0.1.6.1 (2025-08-05)
+
+- Bugfixes: Pin paramiko version to "<4" to maintain compatibility with legacy installations (4891)
+
+
 ## 14.0.1.6.0 (2025-07-30)
 
 - Features: Improve the extendability of the file upload command. (4759)
