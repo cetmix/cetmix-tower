7e45cb3eb884c98fc1f13764590ebe6c127f6416
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-08-14 10:38:43 +0200

    [IMP] Update the website URL in the manifest

--- cetmix_tower_yaml/__manifest__.py
+++ cetmix_tower_yaml/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "17.0.1.3.1",
     "development_status": "Beta",
     "category": "Productivity",
-    "website": "https://cetmix.com",
+    "website": "https://tower.cetmix.com",
     "author": "Cetmix",
     "license": "AGPL-3",
     "installable": True,
