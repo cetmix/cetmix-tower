27f07c7dc475aa8ba49472a3e605e9972005ab94
GabbasovDinar <git.diga@gmail.com>
2025-09-29 12:51:13 +0500

    [ADD] cx_tower_webhook: New module
    - Implements incoming webhooks for Cetmix Tower.
    - Webhooks are authorised using customisable authenticators which can be pre-configured and reused across multiple webhooks.
    - Webhooks and authenticators can be exported and imported using YAML format, which makes them easily sharable.
    
    Task: 4795

---
+++ cetmix_tower_webhook/tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2025 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_cx_tower_webhook_authenticator
+from . import test_cx_tower_webhook_log
+from . import test_cx_tower_webhook
+from . import test_webhook_controller
