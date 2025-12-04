3fa3c8f1bbe033b15f42fa9e5fc236fc027ff85f
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-12-04 22:54:44 +0100

    [IMP] cetmix_tower_git: pre-commit auto fixes

--- cetmix_tower_git/models/cx_tower_git_project_file_template_rel.py
+++ cetmix_tower_git/models/cx_tower_git_project_file_template_rel.py
@@ -103,9 +103,9 @@ class CxTowerGitProjectFileTemplateRel(models.Model):
                             project_format=record.project_format,
                         )
                     )
-                code_generator_functions[
-                    record.project_format
-                ] = code_generator_function
+                code_generator_functions[record.project_format] = (
+                    code_generator_function
+                )
 
             # Generate code for current record
             code = code_generator_function(record)
