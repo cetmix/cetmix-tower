1e5deffb16821b27667d0b0a5a8a2f0440151496
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-12-08 21:57:15 +0100

    Jet cloning initial implementation
    - Functions in models
    - Jet clone wizard

--- cetmix_tower_server/__manifest__.py
+++ cetmix_tower_server/__manifest__.py
@@ -64,6 +64,7 @@
         "wizards/cx_tower_jet_state_wizard_view.xml",
         "wizards/cx_tower_jet_action_wizard_view.xml",
         "wizards/cx_tower_jet_create_wizard_view.xml",
+        "wizards/cx_tower_jet_clone_wizard_view.xml",
         "views/cx_tower_server_view.xml",
         "views/cx_tower_os_view.xml",
         "views/cx_tower_tag_view.xml",
