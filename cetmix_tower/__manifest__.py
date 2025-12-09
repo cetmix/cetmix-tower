b4d986533ad15b025efd405805b1845dac15119c
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-12-09 12:37:14 +0100

    [MIG] cetmix_tower: Migration to 18.0

--- cetmix_tower/__manifest__.py
+++ cetmix_tower/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower",
     "summary": "Odoo SAAS Server Application Management",
-    "version": "17.0.2.1.0",
+    "version": "18.0.1.0.0",
     "development_status": "Beta",
     "category": "Productivity",
     "website": "https://tower.cetmix.com",
