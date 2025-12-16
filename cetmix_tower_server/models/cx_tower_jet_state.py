6e964370e7cb2c345f1b123e5f640c58ff24ed71
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2025-12-16 20:24:19 +0100

    Improved UI/UX for jets.

--- cetmix_tower_server/models/cx_tower_jet_state.py
+++ cetmix_tower_server/models/cx_tower_jet_state.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2024 Cetmix OÜ
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
 
 
 class CxTowerJetState(models.Model):
@@ -16,3 +17,28 @@ class CxTowerJetState(models.Model):
     active = fields.Boolean(default=True)
     color = fields.Integer()
     note = fields.Text()
+
+    def set_state(self, jet=None):
+        """Sets the state of the jet
+
+        Args:
+            jet (cx.tower.jet): Jet to set the state.
+        """
+        self.ensure_one()
+
+        # Try to obtain jet from context if not provided as an argument
+        if jet is None:
+            jet_id = self.env.context.get("jet_id")
+
+            # Just return, no exceptions for now
+            if not jet_id:
+                return
+
+            jet = self.env["cx.tower.jet"].browse(jet_id)
+
+        # Ensure that the state is set for a single jet
+        if not jet or len(jet) > 1:
+            raise ValidationError(_("State can be set only for a single jet"))
+
+        # Bring the jet to the state
+        jet._bring_to_state(self)
