3a5e65d45e2ee3a00f28c68642ba6a98cffe1d5d
CetmixGitBot <gitbot@cetmix.com>
2025-12-26 11:30:02 +0000

    [BOT] post-merge updates

--- cetmix_tower_yaml/__manifest__.py
+++ cetmix_tower_yaml/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Cetmix Tower YAML",
     "summary": "Cetmix Tower YAML export/import",
-    "version": "16.0.2.0.1",
+    "version": "16.0.2.0.2",
     "development_status": "Beta",
     "category": "Productivity",
     "website": "https://tower.cetmix.com",
