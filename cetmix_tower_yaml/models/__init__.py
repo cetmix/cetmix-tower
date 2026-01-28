cc27a60f57b4a5bfda7977e344a757832a5a093a
Ivan Sokolov <2234999+ivs-cetmix@users.noreply.github.com>
2026-01-28 19:42:17 +0100

    [IMP] cetmix_tower_yaml: Waypoints

--- cetmix_tower_yaml/models/__init__.py
+++ cetmix_tower_yaml/models/__init__.py
@@ -23,3 +23,4 @@ from . import cx_tower_jet_template
 from . import cx_tower_jet_template_dependency
 from . import cx_tower_jet_state
 from . import cx_tower_jet_action
+from . import cx_tower_jet_waypoint_template
