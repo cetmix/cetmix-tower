834ea5d7a01732d2abfbd0fb17e5992b9ce541b1
Mario Nuñez <marionumza@gmail.com>
2026-04-17 16:11:17 -0300

    [FIX] cetmix_tower_jet_isolation: enforce strict pylint rules

--- cetmix_tower_jet_isolation/wizards/cx_tower_command_run_wizard.py
+++ cetmix_tower_jet_isolation/wizards/cx_tower_command_run_wizard.py
@@ -1,11 +1,11 @@
 from odoo import models, fields, api
 
 class CxTowerCommandRunWizardFilter(models.TransientModel):
-    _inherit = 'cx.tower.command.run.wizard'
+    _inherit = "cx.tower.command.run.wizard"
 
-    is_isolated_context = fields.Boolean(compute='_compute_is_isolated_context')
+    is_isolated_context = fields.Boolean(compute="_compute_is_isolated_context")
 
-    @api.depends('jet_ids')
+    @api.depends("jet_ids")
     def _compute_is_isolated_context(self):
         for record in self:
             if record.jet_ids and any(j.jet_template_id.isolation_mode for j in record.jet_ids):
@@ -16,14 +16,14 @@ class CxTowerCommandRunWizardFilter(models.TransientModel):
     @api.model
     def default_get(self, fields_list):
         res = super().default_get(fields_list)
-        if 'default_jet_ids' in self.env.context:
-            jet_ids = self.env['cx.tower.jet'].browse(self.env.context['default_jet_ids'])
+        if "default_jet_ids" in self.env.context:
+            jet_ids = self.env["cx.tower.jet"].browse(self.env.context["default_jet_ids"])
             if jet_ids:
                 template = jet_ids[0].jet_template_id
                 
                 if template.isolation_mode:
                     if template.forced_applicability:
-                        res['applicability'] = template.forced_applicability
+                        res["applicability"] = template.forced_applicability
                     if template.forced_command_tag_ids:
-                        res['tag_ids'] = [(6, 0, template.forced_command_tag_ids.ids)]
+                        res["tag_ids"] = [(6, 0, template.forced_command_tag_ids.ids)]
         return res
