e63a03544528ff1442accc0fc338c6a8be95d861
Franco Leyes <leyes404@gmail.com>
2026-04-25 15:57:42 -0300

    [FIX] cetmix_tower_server_terminal: add missing copyright headers

--- cetmix_tower_server_terminal/models/cx_tower_server.py
+++ cetmix_tower_server_terminal/models/cx_tower_server.py
@@ -1,3 +1,4 @@
+# Copyright Cetmix OÜ 2026
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
--- cetmix_tower_server_terminal/models/cx_tower_terminal_session.py
+++ cetmix_tower_server_terminal/models/cx_tower_terminal_session.py
@@ -1,3 +1,4 @@
+# Copyright Cetmix OÜ 2026
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import json
