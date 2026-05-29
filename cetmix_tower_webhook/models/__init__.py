82ee405954832a9b8e72360de2ad2d07443f8d21
Mario Nuñez <marionumza@gmail.com>
2026-05-29 13:53:54 -0300

    upd

--- cetmix_tower_webhook/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2025 Cetmix OÜ
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import cx_tower_webhook_eval_mixin
-from . import cx_tower_webhook_authenticator
-from . import cx_tower_webhook_log
-from . import cx_tower_webhook
-from . import cx_tower_variable
-from . import res_config_settings
