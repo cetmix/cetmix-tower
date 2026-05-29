3784aafe95080022f41d16330b38220e505c3a1a
Mario Osvaldo Nuñez <marionumza@gmail.com>
2026-05-29 13:51:04 -0300

    Delete cetmix_tower_webhook directory
    Signed-off-by: Mario Osvaldo Nuñez <marionumza@gmail.com>

--- cetmix_tower_webhook/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2025 Cetmix OÜ
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import test_cx_tower_webhook_authenticator
-from . import test_cx_tower_webhook_log
-from . import test_cx_tower_webhook
-from . import test_webhook_controller
